63162877e64f527c1c67bb48a17ddb308b48fd0e
kish7 <kishh1729@gmail.com>
2016-09-25 00:11:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/ui/article-one', function (req, res) {
+  res.show('k');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+    
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
